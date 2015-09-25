bc301ce5891e6e4f26f1d5d67f3920bc2c6ffab1
ellmetha <morgan.aubert@zoho.com>
2015-09-25 08:45:49 +0200

    Example project base settings updated

--- example_project/src/example_project/settings/base.py
+++ example_project/src/example_project/settings/base.py
@@ -8,6 +8,13 @@ TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': PROJECT_PATH.child('example.db'),
+    }
+}
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
