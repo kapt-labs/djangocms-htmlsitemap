5ed26819d4f7d4c92bc32ae58b0e4ee864f90db3
pierreben <pierrebenjamin@free.fr>
2016-09-10 11:50:56 +0200

    FEAT: Drop support for django 1.6

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     long_description=read_relative_file('README.rst'),
     zip_safe=False,
     install_requires=[
-        'django>=1.6',
+        'django>=1.7,<1.10',
         'django-cms>=3.1',
     ],
     classifiers=[
